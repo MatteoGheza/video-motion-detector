fbd1989a11eb0fe3d123095e99b0486925783047
Matteo Gheza <matteo.gheza07@gmail.com>
2021-09-02 19:07:02 +0200

    Hide Home and Settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'analyze', component: AnalyzeComponent },
   { path: 'settings', component: SettingsComponent },
   //
-  { path: "**", redirectTo: "/home", pathMatch: "full" }
+  { path: "**", redirectTo: "/analyze", pathMatch: "full" }
 ];
 
 @NgModule({
